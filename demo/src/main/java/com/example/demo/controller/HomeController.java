8e4178e71d91cfe2b882d6ec02c66d54fdfcdd67
wuahanwei <zoefun54@gmail.com>
2022-05-02 16:50:57 +0800

    test

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     
     @RequestMapping("/home") 
     public String home() {
+        // test
         return "home";
     }
 }

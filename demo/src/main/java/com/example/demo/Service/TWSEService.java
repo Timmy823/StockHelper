0ee060879267466da42e577fa9f5242c207f88ed
wuahanwei <zoefun54@gmail.com>
2022-07-12 23:21:30 +0800

    second commit 0712

--- demo/src/main/java/com/example/demo/Service/TWSEService.java
+++ demo/src/main/java/com/example/demo/Service/TWSEService.java
@@ -22,6 +22,10 @@ public class TWSEService {
     ArrayList<String> companyId = new ArrayList<>();
     ArrayList<String> companyName = new ArrayList<>();
 
+    JSONObject status_code = new JSONObject();
+    JSONObject data = new JSONObject();
+    HashMap<String, JSONObject> result = new HashMap<String, JSONObject>();
+
     public TWSEService(String twseUrl) throws IOException{
         this.twseUrl = twseUrl;
     }
@@ -80,16 +84,14 @@ public class TWSEService {
                     }
                 }
             }
-            JSONObject object = new JSONObject();
-            for (int i=0; i<companyId.size();i++){
-                object.put(companyId.get(i),companyName.get(i));
-            }
-            return responseListSuccess(object);
+            return responseSuccessObject();
         }   
         catch (IOException io){
             return responseError(io.toString());
         }
     }
+
+
     /**
      * 建立ssl憑證
      * @param urlObj
@@ -120,40 +122,40 @@ public class TWSEService {
 
         return trust;
     }
+    public JSONObject responseSuccessObject(){
+        putStatusCode("success","");
 
-    private JSONObject responseListSuccess(JSONObject temp_data) {
-        JSONObject status_code = new JSONObject();
-        JSONObject data = new JSONObject();
-        HashMap<String, JSONObject> result = new HashMap<String, JSONObject>();
+        for (int i=0; i<companyId.size();i++){
+            putDataList(companyId.get(i),companyName.get(i));
+        }
+        resultJsonObjectStructure();
+        
+        return new JSONObject(result);
+    }
 
-        status_code.put("status", "success");
-        status_code.put("desc", "");
-
-        data.put("data", temp_data);
-
-        result.put("metadata", status_code);
-        result.put("data", data);
+    private JSONObject responseError(String error_msg) {
+        putStatusCode("error",error_msg);
+        putDataList("data","");
+        resultJsonObjectStructure();
 
         return new JSONObject(result);
+    }
+
+ 
+    private void  putStatusCode(String s,String error_msg){
+        status_code.put("status", s);
+        status_code.put("desc", error_msg);
+    }
+    private void putDataList(String s1,String s2) {
+        data.put(s1,s2);
+        
+    }
+    private void resultJsonObjectStructure() {
+        result.put("data", data);
+        result.put("metadata", status_code);
     }
     
-    private JSONObject responseError(String error_msg) {
-        JSONObject status_code = new JSONObject();
-        JSONObject data = new JSONObject();
-        HashMap<String, JSONObject> result = new HashMap<String, JSONObject>();
-
-        status_code.put("status", "error");
-        status_code.put("desc", error_msg);
-
-        data.put("data", new JSONObject());
-
-        result.put("metadata", status_code);
-        result.put("data", data);
-
-        return new JSONObject(result);
-    }
-
-
-
-
+    
 }
+
+

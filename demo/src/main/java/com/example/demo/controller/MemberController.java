c10c2132615c80a96ecd2b9db2d96975c2e305c7
wuahanwei <zoefun54@gmail.com>
2022-08-30 14:55:13 +0800

    add @CrossOrigin

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -6,9 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.Component.CreateLoginParam;
 import com.example.demo.Service.MemberService;
@@ -19,6 +17,7 @@ public class MemberController {
     @Autowired
     MemberService memberService;
 
+    @CrossOrigin(origins = "http://localhost:5277", allowedHeaders = "")
     @PostMapping("/member/searchMember")
     public JSONObject CheckMemberAndCreateLogin(@Valid @RequestBody CreateLoginParam input) {
         try{

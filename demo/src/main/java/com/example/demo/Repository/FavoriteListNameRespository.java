1bd6864900022d8020c4b1a7d2910acf5d97ba33
wuahanwei <zoefun54@gmail.com>
2022-09-03 17:26:51 +0800

    delete not use code

--- demo/src/main/java/com/example/demo/Repository/FavoriteListNameRespository.java
+++ demo/src/main/java/com/example/demo/Repository/FavoriteListNameRespository.java
@@ -8,9 +8,4 @@ import com.example.demo.Entity.FavoriteListNameModel;
 public interface FavoriteListNameRespository extends JpaRepository<FavoriteListNameModel, Long> {
     @Query(nativeQuery = true, value = "select * from favorite_list_name where member_id = ?1 and BINARY list_name = ?2")
     public FavoriteListNameModel FindByListName(String member_id, String list_name);  
-
-    // @Modifying
-    // @Transactional
-    // @Query(nativeQuery = true, value = "UPDATE favorite_list_name SET status = ?3 WHERE member_id = ?1 and BINARY list_name = ?2")
-    // public void updateListStatusByListName(String mid, String list_name, String status);  
 }

d27665e853c36c31eeec361adcc26da2644b4b1e
wuahanwei <zoefun54@gmail.com>
2022-09-03 17:27:52 +0800

    query add binary

--- demo/src/main/java/com/example/demo/Repository/FavoriteListNameRespository.java
+++ demo/src/main/java/com/example/demo/Repository/FavoriteListNameRespository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.demo.Entity.FavoriteListNameModel;
 
 public interface FavoriteListNameRespository extends JpaRepository<FavoriteListNameModel, Long> {
-    @Query(nativeQuery = true, value = "select * from favorite_list_name where member_id = ?1 and BINARY list_name = ?2")
+    @Query(nativeQuery = true, value = "select * from favorite_list_name where BINARY member_id = ?1 and BINARY list_name = ?2")
     public FavoriteListNameModel FindByListName(String member_id, String list_name);  
 }

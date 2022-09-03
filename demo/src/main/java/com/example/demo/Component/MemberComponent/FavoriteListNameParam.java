cbdf536b36d3e384ef705def451896da01638867
wuahanwei <zoefun54@gmail.com>
2022-09-03 17:26:07 +0800

    list name param validator add NotNull

--- demo/src/main/java/com/example/demo/Component/MemberComponent/FavoriteListNameParam.java
+++ demo/src/main/java/com/example/demo/Component/MemberComponent/FavoriteListNameParam.java
@@ -1,6 +1,7 @@
 package com.example.demo.Component.MemberComponent;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -12,6 +13,7 @@ public class FavoriteListNameParam {
     private String account;
 
     @NotEmpty(message = "favorite_list_name不可為空")
+    @NotNull(message = "favorite_list_name不可為null")
     @Size(max = 50, message="favorite_list_name最長50")
     private String list_name;
     
